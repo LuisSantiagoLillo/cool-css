8e96385c8ef16914de4f1e2e514744e6b896138a
Luis <luisslillo_90@hotmail.es>
2019-12-19 15:55:55 +0000

    modify form in driving theme

--- src/components/logins/car_driving/CarDriving.jsx
+++ src/components/logins/car_driving/CarDriving.jsx
@@ -52,22 +52,15 @@ class CarDriving extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
-                            <form className="container-form p-5">
-                                <div className="text-center">
-                                    <h4 className="title_app">MOTOR INSURANCES</h4>
-                                </div>
-                                <div className="form-group">
-                                    <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@gmail.com"></input>
-                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                                </div>
-                                <div className="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="******"></input>
-                                </div>
-                        
-                                <button type="button" className="btn btn-primary btn-block mt-5">Submit</button>
-                            </form>
+                            <div class="box mt-5">
+                                <form className="signup-form" action="index.html" method="post">
+                                    <h1>Motor Insurances</h1>
+                                    <input type="email" placeholder="Email" className="txtb"/>
+                                    <input type="password" placeholder="Password" className="txtb"/>
+                                    <input type="button" value="Sign In" className="signup-btn"/>
+                                    <a href="#">Or create account</a>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
